b35de497aec084316db13858e7db91ee47fd3296
Rishabh23-web <rishabhtewari23@gmail.com>
2026-04-07 18:58:40 +0530

    Time: 16 ms (59.66%) | Memory: 47.4 MB (43.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,46 +1,31 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-        int n =s.length();
-
-        if(t.length()>n)return "";
-
+        if(t.length()>s.length())return "";
         Map<Character,Integer> map = new HashMap<>();
-
-        for(char ch:t.toCharArray())
+        for(char ch: t.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
-
+        }
         int reqCount = t.length();
         int i=0,j=0;
-
         int minWinSize = Integer.MAX_VALUE ;
         int st_i =0;
-
-        while(j<n){
+        while(j<s.length()){
             char ch = s.charAt(j);
-
-            if(map.containsKey(ch) && map.get(ch)>0)reqCount--;
-
+            if(map.containsKey(ch)&& map.get(ch)>0)reqCount--;
             map.put(ch,map.getOrDefault(ch,0)-1);
-
             while(reqCount==0){
-
                 int currWinSize = j-i+1;
-
-                if(currWinSize< minWinSize){
-                    minWinSize=currWinSize;
-                    st_i = i;
+                if(currWinSize<minWinSize){
+                    minWinSize =  currWinSize;
+                    st_i=i;
                 }
-
-                char startChar= s.charAt(i);
-                map.put(startChar,map.getOrDefault(startChar,0)+1);
-
-                if(map.containsKey(startChar) && map.get(startChar)>0)reqCount++;
+                char start = s.charAt(i);
+                map.put(start,map.getOrDefault(start,0)+1);
+                if(map.containsKey(start) && map.get(start)>0)reqCount++;
                 i++;
-            }
-            j++;
-        }
-        return minWinSize==Integer.MAX_VALUE?"":s.substring(st_i,st_i+minWinSize);
+            }j++;
+        }return  minWinSize==Integer.MAX_VALUE?"":s.substring(st_i,st_i+minWinSize);       
 
     }
 }
