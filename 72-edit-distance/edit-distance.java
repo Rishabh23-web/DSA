c8b719997d9b4ecf43724c36de37b0915626a7b0
Rishabh23-web <rishabhtewari23@gmail.com>
2026-04-25 19:30:35 +0530

    Time: 5 ms (68.98%) | Memory: 47.2 MB (53.60%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,18 @@
 class Solution {
-    public int findLCS(int t[][],int m, int n,String w1,String w2){
+    public int minDistance(String s1, String s2) {
+        final int m=s1.length();
+        final int n = s2.length();
+        int [][] t = new int[m+1][n+1];
         for(int i=0;i<=m;i++)t[i][0]=i;
-        for(int j=0;j<=n;j++)t[0][j]=j;
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
+        for(int i=0;i<=n;i++)t[0][i]=i;
 
-                if(w1.charAt(i-1)==w2.charAt(j-1))t[i][j]= t[i-1][j-1];
-                else t[i][j] = 1 + Math.min(t[i - 1][j - 1], // Replace
-                                  Math.min(t[i - 1][j],    // Delete
-                                           t[i][j - 1]));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) t[i][j]=t[i-1][j-1];
+                else{
+                    t[i][j] =1+ Math.min(t[i-1][j-1],Math.min(t[i][j-1],t[i-1][j]));
+                }
             }
         }return t[m][n];
-    }
-    public int minDistance(String word1, String word2) {
-        int m =word1.length();
-        int n =word2.length();
-        int t[][]=new int[m+1][n+1];
-        int lcs = findLCS(t,m,n,word1,word2);
-        return lcs;
-
-        
     }
 }
